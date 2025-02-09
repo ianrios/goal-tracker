0e573fe05992b76a4b83fa99c4741dd92536ee70
Ian Rios <ian.rios1a@gmail.com>
2025-02-09 11:45:55 -0500

    created helper methods

--- src/pages/api/goals/create-goal.ts
+++ src/pages/api/goals/create-goal.ts
@@ -28,7 +28,54 @@ export default async function handler(
 }
 
 export async function insertOne(goal: Goal, res: NextApiResponse) {
-  const queryString = `INSERT INTO ${goalsTable.name} (id, createdAt, updatedAt, completedAt, archivedAt, deletedAt, deadline, header, body, bucket) values ('${goal.id}', '${goal.createdAt}', '${goal.updatedAt}', '${goal.completedAt}', '${goal.archivedAt}', '${goal.deletedAt}', '${goal.deadline}', '${goal.header}', '${goal.body}', '${goal.bucket}')`;
+  const columnKeys = Object.keys(goal);
+  const columns = columnKeys.reduce(
+    (a, c) => (a.length === 0 ? c : `${a}, ${c}`),
+    ""
+  );
+  // const columns = `id, createdAt, updatedAt, completedAt, archivedAt, deletedAt, deadline, header, body, bucket`;
+  const value = columnKeys.reduce(
+    (a, c) =>
+      a.length === 0
+        ? `'${goal[c as keyof Goal]}'`
+        : `${a}, '${goal[c as keyof Goal]}'`,
+    ""
+  );
+  // const values = `'${goal.id}', '${goal.createdAt}', '${goal.updatedAt}', '${goal.completedAt}', '${goal.archivedAt}', '${goal.deletedAt}', '${goal.deadline}', '${goal.header}', '${goal.body}', '${goal.bucket}'`;
+  const queryString = `INSERT INTO ${goalsTable.name} (${columns}) VALUES (${value})`;
+  const result = await query({ queryString, startLog: "Inserting new goal" });
+
+  res.status(200).json(result);
+}
+
+export async function insertMany(goals: Goal[], res: NextApiResponse) {
+  /*
+  insert into table_name (user_id, subservice_id) values
+  (1, 1),
+  (1, 2),
+  (1, 3),
+  (2, 1);
+
+  */
+  const columnKeys = Object.keys(goal);
+  const columns = columnKeys.reduce(
+    (a, c) => (a.length === 0 ? c : `${a}, ${c}`),
+    ""
+  );
+  const values = goals.reduce((a, goal) => {
+    const value = columnKeys.reduce(
+      (a, c) =>
+        a.length === 0
+          ? `'${goal[c as keyof Goal]}'`
+          : `${a}, '${goal[c as keyof Goal]}'`,
+      ""
+    );
+    return a.length === 0 ? `(${value})` : `${a}, (${value})`;
+  }, "");
+
+  const queryString = `INSERT INTO ${goalsTable.name} (${columns}) VALUES ${values}`;
+
+  // const queryString = `INSERT INTO ${goalsTable.name} (id, createdAt, updatedAt, completedAt, archivedAt, deletedAt, deadline, header, body, bucket) values ('${goal.id}', '${goal.createdAt}', '${goal.updatedAt}', '${goal.completedAt}', '${goal.archivedAt}', '${goal.deletedAt}', '${goal.deadline}', '${goal.header}', '${goal.body}', '${goal.bucket}')`;
   const result = await query({ queryString, startLog: "Inserting new goal" });
 
   res.status(200).json(result);
